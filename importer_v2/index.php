e6114e48a08ad2af3d3558ef152a265c53cd30ed
Jorge H. F. Pereira <jhfpereira@users.noreply.github.com>
2019-06-24 22:28:20 +0200

    Export von sprachgruppierten Grafik-Objekten (Datei pro Sprache)

--- importer_v2/index.php
+++ importer_v2/index.php
@@ -4,18 +4,18 @@ require_once 'Language.php';
 require_once 'process/Pipeline.php';
 require_once 'collectors/GraphicsInventory.php';
 require_once 'importers/GraphicsXMLImporter.php';
-require_once 'exporters/GraphicsJSONExporter.php';
+require_once 'exporters/GraphicsJSONLangExporter.php';
 
 use CranachImport\Process\Pipeline;
 use CranachImport\Collectors\GraphicsInventory;
 use CranachImport\Importers\GraphicsXMLImporter;
-use CranachImport\Exporters\GraphicsJSONExporter;
+use CranachImport\Exporters\GraphicsJSONLangExporter;
 
 $graphicsXMLSourceFilePath = '../importer/import-file/20190328/CDA-GR_DatenÅbersicht_20190329.xml';
-$graphicsJSONDestinationPath = './cda-graphics-v2.json';
+$graphicsJSONDestinationPath = './output/cda-graphics-v2.json';
 
 $graphicsXmlImporter = new GraphicsXMLImporter($graphicsXMLSourceFilePath);
-$graphicsJsonExporter = new GraphicsJSONExporter($graphicsJSONDestinationPath);
+$graphicsJsonExporter = new GraphicsJSONLangExporter($graphicsJSONDestinationPath);
 
 $pipe = new Pipeline;
 $pipe->addExporter($graphicsJsonExporter);
