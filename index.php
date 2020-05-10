82eb10366ed1e115e991f2d396258432c844bd3d
Jorge H. F. Pereira <jhfpereira@users.noreply.github.com>
2020-05-11 00:57:17 +0200

    refactor: use pipleline instance to trigger import

--- index.php
+++ index.php
@@ -5,16 +5,21 @@ namespace CranachDigitalArchive\Importer;
 require_once __DIR__ . '/vendor/autoload.php';
 
 use CranachDigitalArchive\Importer\Modules\Graphics\Exporters\GraphicsJSONLangExistenceTypeExporter;
-use CranachDigitalArchive\Importer\Modules\Graphics\Transformers\ConditionDeterminer;
-use CranachDigitalArchive\Importer\Modules\Graphics\Transformers\RemoteImageExistenceChecker;
+use CranachDigitalArchive\Importer\Modules\Graphics\Transformers\{ConditionDeterminer, RemoteImageExistenceChecker};
 use CranachDigitalArchive\Importer\Modules\Graphics\Loaders\XML\GraphicsLoader;
+use CranachDigitalArchive\Importer\Pipeline\Pipeline;
 
-$loader = GraphicsLoader::withSourceAt('./input/20191122/CDA-GR_Datenuebersicht_20191122.xml');
-$node = $loader->pipe(
-	RemoteImageExistenceChecker::withCacheAt('./.cache'),
-	ConditionDeterminer::new(),
+$graphicsLoader = GraphicsLoader::withSourceAt('./input/20191122/CDA-GR_Datenuebersicht_20191122.xml');
+$remoteImageExistenceChecker = RemoteImageExistenceChecker::withCacheAt('./.cache');
+$conditionDeterminer = ConditionDeterminer::new();
+$destination = GraphicsJSONLangExistenceTypeExporter::withDestinationAt('./output/20191122/cda-graphics-v2.json');
+
+$graphicsLoader->pipe(
+	$remoteImageExistenceChecker,
+	$conditionDeterminer,
+	$destination,
 );
-$node->pipe(GraphicsJSONLangExistenceTypeExporter::withDestinationAt('./output/20191122/cda-graphics-v2.json'));
 
-
-$loader->run();
+Pipeline::new()->addNodes(
+	$graphicsLoader,
+)->start();
