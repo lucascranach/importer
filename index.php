cc9be7dd81473afe541be3c22b3317db8da93426
Jorge H. F. Pereira <jhfpereira@users.noreply.github.com>
2020-12-06 15:59:17 +0100

    chore: generate new jsons for 20201118

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ use CranachDigitalArchive\Importer\Modules\Thesaurus\Loaders\XML\ThesaurusLoader
 use CranachDigitalArchive\Importer\Modules\Thesaurus\Exporters\ThesaurusJSONExporter;
 use CranachDigitalArchive\Importer\Modules\Thesaurus\Exporters\ThesaurusMemoryExporter;
 
-$date = '20201009';
+$date = '20201118';
 $inputDirectory = './input/' . $date;
 $destDirectory = './docs/' . $date;
 
