23403f194d0ecf33da3865d6de0a33ab8f26f49b
Jorge H. F. Pereira <jhfpereira@users.noreply.github.com>
2021-05-21 17:46:46 +0200

    feat(paintings): extend filter value transformer for anonymous masters

--- src/Modules/Paintings/Transformers/ExtenderWithBasicFilterValues.php
+++ src/Modules/Paintings/Transformers/ExtenderWithBasicFilterValues.php
@@ -50,6 +50,137 @@ class ExtenderWithBasicFilterValues extends Hybrid
             ],
         ],
 
+        /* Anonymous masters -> Begin */
+
+        'attribution.named_masters_from_the_cranach_workshop.antonius_heusler' => [
+            'name' => [
+                Language::DE => 'Antonius Heusler',
+                Language::EN => 'Antonius Heusler',
+            ],
+        ],
+
+        'attribution.named_masters_from_the_cranach_workshop.augustus_cordes' => [
+            'name' => [
+                Language::DE => 'Augustus Cordes',
+                Language::EN => 'Augustus Cordes',
+            ],
+        ],
+
+        'attribution.named_masters_from_the_cranach_workshop.hans_doering' => [
+            'name' => [
+                Language::DE => 'Hans Döring',
+                Language::EN => 'Hans Döring',
+            ],
+        ],
+
+        'attribution.named_masters_from_the_cranach_workshop.hans_kemmer' => [
+            'name' => [
+                Language::DE => 'Hans Kemmer',
+                Language::EN => 'Hans Kemmer',
+            ],
+        ],
+
+        'attribution.named_masters_from_the_cranach_workshop.hans_maler' => [
+            'name' => [
+                Language::DE => 'Hans Maler',
+                Language::EN => 'Hans Maler',
+            ],
+        ],
+
+        'attribution.named_masters_from_the_cranach_workshop.heinrich_vogtherr' => [
+            'name' => [
+                Language::DE => 'Heinrich Vogtherr',
+                Language::EN => 'Heinrich Vogtherr',
+            ],
+        ],
+
+        'attribution.named_masters_from_the_cranach_workshop.master_h_b__with_a_griffin_s_head' => [
+            'name' => [
+                Language::DE => 'Meister H\.B\. mit dem Greifenkopf',
+                Language::EN => 'Master H\.B\. with a griffin\'s head',
+            ],
+        ],
+
+        'attribution.named_masters_from_the_cranach_workshop.master_i_s_' => [
+            'name' => [
+                Language::DE => 'Meister I\.S\.',
+                Language::EN => 'Master I\.S\.',
+            ],
+        ],
+
+        'attribution.named_masters_from_the_cranach_workshop.master_of_the_mass_of_st_gregory' => [
+            'name' => [
+                Language::DE => 'Meister der Gregorsmesse',
+                Language::EN => 'Master of the Mass of St Gregory',
+            ],
+        ],
+
+        'attribution.named_masters_from_the_cranach_workshop.master_of_the_doebeln_altarpiece' => [
+            'name' => [
+                Language::DE => 'Meister des Döbelner Hochaltars',
+                Language::EN => 'Master of the Döbeln Altarpiece',
+            ],
+        ],
+
+        'attribution.named_masters_from_the_cranach_workshop.master_of_the_pflock_altarpiece' => [
+            'name' => [
+                Language::DE => 'Meister des Pflockschen Altars',
+                Language::EN => 'Master of the Pflock Altarpiece',
+            ],
+        ],
+
+        'attribution.named_masters_from_the_cranach_workshop.master_monogramist_i_w_' => [
+            'name' => [
+                Language::DE => 'Meister\/Monogrammist I\.W\.',
+                Language::EN => 'Master\/Monogramist I\.W\.',
+            ],
+        ],
+
+        'attribution.named_masters_from_the_cranach_workshop.matthias_gruenewald' => [
+            'name' => [
+                Language::DE => 'Matthias Grünewald',
+                Language::EN => 'Matthias Grünewald',
+            ],
+            'prefix' => [
+                Language::DE => '',
+                Language::EN => '',
+            ],
+        ],
+
+        'attribution.named_masters_from_the_cranach_workshop.peter_rodelstedt' => [
+            'name' => [
+                Language::DE => 'Peter Rodelstedt',
+                Language::EN => 'Peter Rodelstedt',
+            ],
+        ],
+
+        'attribution.named_masters_from_the_cranach_workshop.pseudo_matthias_gruenewald' => [
+            'name' => [
+                Language::DE => 'Matthias Grünewald',
+                Language::EN => 'Matthias Grünewald',
+            ],
+            'prefix' => [
+                Language::DE => 'Pseudo-',
+                Language::EN => 'Pseudo-',
+            ],
+        ],
+
+        'attribution.named_masters_from_the_cranach_workshop.simon_franck' => [
+            'name' => [
+                Language::DE => 'Simon Franck',
+                Language::EN => 'Simon Franck',
+            ],
+        ],
+
+        'attribution.named_masters_from_the_cranach_workshop.wolfgang_krodel_the_elder' => [
+            'name' => [
+                Language::DE => 'Wolfgang Krodel d\. Ä\.',
+                Language::EN => 'Wolfgang Krodel the Elder',
+            ],
+        ],
+
+        /* Anonymous masters -> End */
+
         'attribution.follower_of_lucas_cranach_the_elder' => [
             'suffix' => [
                 Language::DE => '.*nachahmer.*',
