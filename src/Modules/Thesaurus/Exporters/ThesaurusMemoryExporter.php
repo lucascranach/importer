f7aa79b3fdd7b5d0c1ee2fd33a44326e2abcc1b8
Jorge H. F. Pereira <jhfpereira@users.noreply.github.com>
2021-05-31 19:38:51 +0200

    feat(thesaurus): introduce check routines for thesaurus data

--- src/Modules/Thesaurus/Exporters/ThesaurusMemoryExporter.php
+++ src/Modules/Thesaurus/Exporters/ThesaurusMemoryExporter.php
@@ -14,6 +14,8 @@ use CranachDigitalArchive\Importer\Pipeline\Consumer;
  */
 class ThesaurusMemoryExporter extends Consumer implements IMemoryExporter
 {
+    const ALT_NAME_DKULT_TERM_IDENTIFIER = 'dkultTermIdentifier';
+
     private $item = null;
     private $done = false;
 
@@ -40,6 +42,18 @@ class ThesaurusMemoryExporter extends Consumer implements IMemoryExporter
         }
 
         $this->item = $item;
+
+        $duplicateItemIds = $this->getDuplicatesItemIds($item);
+        if (count($duplicateItemIds) > 0) {
+            $this->outputDuplicateItemIds($duplicateItemIds);
+        }
+
+        $altFieldName = self::ALT_NAME_DKULT_TERM_IDENTIFIER;
+
+        $itemsWithMissingAltField = $this->getItemsWithMissingAltField($item, $altFieldName);
+        if (count($itemsWithMissingAltField) > 0) {
+            $this->outputItemsWithMissingAltField($itemsWithMissingAltField, $altFieldName);
+        }
 
         return true;
     }
@@ -76,6 +90,84 @@ class ThesaurusMemoryExporter extends Consumer implements IMemoryExporter
         }
 
         return null;
+    }
+
+
+    private function getDuplicatesItemIds(Thesaurus $item): array
+    {
+        $rootTerms = $item->getRootTerms();
+        $ids = $this->collectIds($rootTerms);
+
+        $duplicateIds = [];
+
+        foreach ($ids as $key => $val) {
+            if ($val > 1) {
+                $duplicateIds[] = $key;
+            }
+        }
+
+        return $duplicateIds;
+    }
+
+
+    private function collectIds($terms, array &$ids = []): array
+    {
+        foreach ($terms as $term) {
+            $termId = $term->getAlt(self::ALT_NAME_DKULT_TERM_IDENTIFIER);
+
+            if (is_null($termId)) {
+                return $ids;
+            }
+
+            if (!isset($ids[$termId])) {
+                $ids[$termId] = 0;
+            }
+
+            $ids[$termId] += 1;
+
+            $this->collectIds($term->getSubTerms(), $ids);
+        }
+
+        return $ids;
+    }
+
+
+    private function outputDuplicateItemIds(array $duplicateItemIds)
+    {
+        echo "\nDuplicate Thesaurus item ids: " . implode(', ', $duplicateItemIds) . "\n\n";
+    }
+
+
+    private function getItemsWithMissingAltField(Thesaurus $item, string $altFieldName): array
+    {
+        $rootTerms = $item->getRootTerms();
+
+        return $this->collectItemsWithMissingAltField($rootTerms, $altFieldName);
+    }
+
+
+    public function collectItemsWithMissingAltField(array $items, string $altFieldName, array &$terms = [], string $concatTermName = ''): array
+    {
+        foreach ($items as $item) {
+            $altFieldValue = $item->getAlt($altFieldName);
+
+            $newConcatTermName = $concatTermName . ' > ' . $item->getTerm();
+
+            if (is_null($altFieldValue)) {
+                $terms[] = $newConcatTermName;
+            }
+
+            $this->collectItemsWithMissingAltField($item->getSubTerms(), $altFieldName, $terms, $newConcatTermName);
+        }
+
+        return $terms;
+    }
+
+
+    private function outputItemsWithMissingAltField(array $itemsWithMissingAltField, string $altFieldName)
+    {
+        echo "Thesaurus items with missing alt field \"" . $altFieldName . "\" :\n  * ";
+        echo implode("\n  * ", $itemsWithMissingAltField) . "\n\n";
     }
 
 
