5c65ff161b95af946c5b445a20d8412bd1d8d34c
Jorge H. F. Pereira <jhfpereira@users.noreply.github.com>
2022-01-28 11:21:48 +0100

    chore(deps): upgrade php-cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,11 +4,10 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
 ;
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR2' => true,
         'strict_param' => true,
-        'array_syntax' => ['syntax' => 'short'],
     ])
    	->setRiskyAllowed(true)
     ->setFinder($finder)
