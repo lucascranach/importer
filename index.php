b3a03d60b5c0a41431e46f96eabb628b98c62e40
Jorge H. F. Pereira <jhfpereira@users.noreply.github.com>
2022-05-14 17:51:02 +0200

    chore(data): import of new export data (20220512)

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ use CranachDigitalArchive\Importer\Modules\Filters\Exporters\CustomFiltersMemory
 use CranachDigitalArchive\Importer\Modules\Filters\Loaders\Memory\CustomFiltersAndThesaurusLoader;
 use CranachDigitalArchive\Importer\Modules\Filters\Transformers\AlphabeticSorter;
 
-$date = '20220504';
+$date = '20220512';
 $inputDirectory = './input/' . $date;
 $destDirectory = './docs/' . $date;
 $filtersDirectory = './filters';
